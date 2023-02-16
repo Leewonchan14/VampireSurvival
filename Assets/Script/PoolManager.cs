655b5fadb1120bdae9de041b2f795758239c53a8
이원찬 <105588857+Leewonchan14@users.noreply.github.com>
2023-02-16 16:00:24 +0900

    디버깅위해 클론 객체마다 번호 부여

--- Assets/Script/PoolManager.cs
+++ Assets/Script/PoolManager.cs
@@ -32,6 +32,7 @@ public class PoolManager : MonoBehaviour
         if(!select){
         // 새롭게 생성하고 select 변수에 할당
             select = Instantiate(prefabs[index], transform);
+            select.name += pools[index].Count;
             pools[index].Add(select);
         }
 
