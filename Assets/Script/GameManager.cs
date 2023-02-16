dc6db71cc950a0b30deea12c8414a15add01ec47
이원찬 <105588857+Leewonchan14@users.noreply.github.com>
2023-02-16 16:28:54 +0900

    일정수준의 경험치일때 레벨업하기!

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -5,8 +5,14 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+    [Header("# Game Object")]
     public player player;
     public PoolManager poolManager;
+    [Header("# Player info")]
+    public int level;
+    public int kill;
+    public int exp;
+    public int[] nextExp = {3,5,10,100,150,210,280,360,450,550};
 
     // Start is called before the first frame update
     
@@ -15,5 +21,12 @@ public class GameManager : MonoBehaviour
     }
     void Update() {
         
+    }
+    public void GetExp(){
+        exp++;
+        if(exp==nextExp[level]){
+            level++;
+            exp = 0;
+        }
     }
 }
